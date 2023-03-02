1f7c2f66ef9d77b2f9ebcbd92a2f700d76218e49
Knut Kirkhorn <knutkirk@hotmail.com>
2023-03-02 19:13:58 +0100

    Replace df command with mount command

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ import {promisify} from 'node:util';
 import {exec} from 'node:child_process';
 
 const execAsync = promisify(exec);
-const commands = {
-	win32: 'echo %SystemDrive%',
-	darwin: 'df -l | awk \'$9 == "/"\''
-};
 
 export default async function systemDisk() {
-	const command = process.platform in commands
-		? commands[process.platform]
-		: 'df -l | awk \'$6 == "/"\'';
+	const command = process.platform === 'win32'
+		? 'echo %SystemDrive%'
+		: 'mount | awk \'$3 == "/"\'';
 	const {error, stdout, stderr} = await execAsync(command);
 
 	if (error || stderr) {
@@ -21,9 +17,5 @@ export default async function systemDisk() {
 		return stdout.split('\r\n')[0];
 	}
 
-	if (process.platform === 'darwin') {
-		return stdout.split('  ')[0];
-	}
-
-	return stdout.split('   ')[0];
+	return stdout.split('\n')[0];
 }
